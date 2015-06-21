9b7f2658c2c4a2d6ac5d7620674a81ba5d262266
Bernie Pope <florbitous@gmail.com>
2015-06-21 17:35:09 +1000

    Replaced Cancer with Twin ion in the program output message

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ DEFAULT_CONFIG_FILE = 'pipeline.config'
 
 def parse_command_line():
     '''Parse the command line arguments of the pipeline'''
-    parser = cmdline.get_argparse(description='Colorectal cancer pipeline',
+    parser = cmdline.get_argparse(description='Twin ion pipeline',
         ignored_args = ["version"] )
     parser.add_argument('--config', type=str, default=DEFAULT_CONFIG_FILE,
         help='Pipeline configuration file in YAML format, defaults to {}' \
